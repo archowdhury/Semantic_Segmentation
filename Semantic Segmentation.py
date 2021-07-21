b04e480dc782c08ee27de96e2a4335646a7b60d2
Amit Rai Chowdhury <amit.raichowdhury@gmail.com>
2021-07-21 20:33:40 +0530

    Added the code to load the images and masks

--- Semantic Segmentation.py
+++ Semantic Segmentation.py
@@ -13,8 +13,9 @@ from skimage.io import imread, imshow, imread_collection, concatenate_images
 from skimage.transform import resize
 from skimage.morphology import label
 from functools import reduce
+from tqdm import tqdm
 
-warnings.filterwarnings('ignore', category=UserWarning, module='skimage')
+warnings.filterwarnings('ignore')
 
 #================================================================#
 # DEFINE THE CONSTANTS
@@ -26,24 +27,46 @@ TRAIN_PATH = 'D:/Machine Learning/Computer Vision/U_NET/train/'
 TEST_PATH = 'D:/Machine Learning/Computer Vision/U_NET/validation/'
 
 
-curdir = os.path.join(TRAIN_PATH, os.listdir(TRAIN_PATH)[6])
+#================================================================#
+# READ IN THE IMAGES AND MASKS
+#================================================================#
 
-for (dirpath, dirname, filenames) in os.walk(curdir):
-    if 'images' in dirpath:
-       image = os.path.join(dirpath, filenames[0])
-    if 'masks' in dirpath:
-        masks = [os.path.join(dirpath, file) for file in filenames]
+def load_images(folder, mode='test'):
 
-image = read_image(image)
+    X, y = [], []
 
-final_mask = np.zeros((IMG_WIDTH, IMG_HEIGHT, 3))
-for i in masks:
-    final_mask += read_image(i)
+    for i in tqdm(os.listdir(folder)):
+        # Read in the image file
+        path, _, file = next(os.walk(folder + i + '/images'))
+        image_file = path + '/' + file[0]
+        img = cv2.imread(image_file)
+        img = cv2.resize(img, (IMG_WIDTH, IMG_HEIGHT))
+        X.append(img)
 
-cv2.imshow('Image', image)
-cv2.imshow('Mask', final_mask)
+        if mode == 'train':
+            # Read in and concatenate the masks
+            path, _, files = next(os.walk(folder + i + '/masks'))
+            masks = [path + '/' + f for f in files]
+
+            final_mask = np.zeros((IMG_WIDTH, IMG_HEIGHT, 1))
+            for mask in masks:
+                mask_img = cv2.imread(mask)
+                mask_img = cv2.resize(mask_img, (IMG_WIDTH, IMG_HEIGHT))
+                mask_img = np.expand_dims(cv2.cvtColor(mask_img, cv2.COLOR_BGR2GRAY), axis=-1)
+                final_mask += mask_img
+            y.append(final_mask)
+
+    return np.asarray(X), np.asarray(y)
+
+X_train, y_train = load_images(TRAIN_PATH, mode='train')
+X_test, _ = load_images(TEST_PATH, mode='test')
+
+
+# Check if the images are loading properly
+idx = 4
+cv2.imshow('image', X_train[idx])
+cv2.imshow('mask', y_train[idx])
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
 
 
